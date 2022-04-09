c8f2cb3c7872f3da02542c8a0b88255e7eceb860
Flian <56669890+FlianLiu@users.noreply.github.com>
2022-04-10 00:48:27 +0800

    all complete v.4
    without RWD

--- js/index.js
+++ js/index.js
@@ -22,33 +22,3 @@ imgWrapper.addEventListener('click', function() {
   imgWrapper.classList.add(`img${targetImg}`);
   btns[targetImg-1].classList.add('show');
 })
-
-//decoration
-const items = document.getElementsByClassName("trans");
-let itemsPos = [];
-let mousePos;
-for (let i=0; i<items.length; i++) {
-  let item = items[i];
-  let itemPos = {
-    x: item.offsetLeft + (item.offsetWidth / 2),
-    y: item.offsetTop + (item.offsetHeight / 2 )
-  }
-  itemsPos[i] = itemPos;
-  items[i].style.transform = `translateX(0px) translateY(0px)`;
-}
-window.addEventListener('mousemove', e=> {
-  mousePos = {
-    x: e.pageX,
-    y: e.pageY
-  };
-  for (let i=0; i<items.length; i++) {
-    let ratio = items[i].dataset.ratio;
-    let delta_x = -(mousePos.x - itemsPos[i].x)*0.003*ratio;
-    let delta_y = -(mousePos.y - itemsPos[i].y)*0.003*ratio;
-    let delta = {
-      x: delta_x,
-      y: delta_y
-    }
-    items[i].style.transform = `translateX(${delta.x}px) translateY(${delta.y}px)`;
-    }
-})
